d63ed0bd49e6c1042b18d1f579b2a6c68bde3093
Vitalii <vitaliti.yurchenko@gmail.com>
2024-09-30 00:54:05 +0300

    Added conversion from Fahrenheit to Celsius

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,9 +1,25 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.print("***Конвертер температури***");
+        Scanner scanner = new Scanner(System.in);
+        double fahrenheit;
+        double celsius;
+
+        System.out.print("***Конвертер температури***" + "\nВведіть температуру в Фаренгейтах: ");
+        fahrenheit = scanner.nextDouble();
+
+        celsius = convFahrenheitToCelsius(fahrenheit);
+
+        System.out.printf("\nВ %s фаренгейтах ~%.2f градусів по Цельсію.", fahrenheit, celsius);
+
+    }
+
+    private static double convFahrenheitToCelsius (double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
     }
 }
