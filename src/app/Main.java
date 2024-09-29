1a73e19b1edee64f4915f8a7d6cc94f6d3fe47d6
Vitalii <vitaliti.yurchenko@gmail.com>
2024-09-30 01:39:21 +0300

    Added conversion from degrees Celsius to degrees Fahrenheit

--- src/app/Main.java
+++ src/app/Main.java
@@ -15,11 +15,21 @@ public class Main {
 
         celsius = convFahrenheitToCelsius(fahrenheit);
 
-        System.out.printf("\nВ %s фаренгейтах ~%.2f градусів по Цельсію.", fahrenheit, celsius);
+        System.out.printf("\nВ %s градусах по Фаренгейту ~%.2f градусів по Цельсію.", fahrenheit, celsius);
+        System.out.print("\nВведіть температуру в Цельсіях: ");
+        celsius = scanner.nextDouble();
+
+        fahrenheit = convCelsiusToFahrenheit(celsius);
+
+        System.out.printf("\nВ %s градусах по Цельсію ~%.2f градусів по Фаренгейту.", celsius, fahrenheit);
 
     }
 
     private static double convFahrenheitToCelsius (double fahrenheit) {
         return (fahrenheit - 32) * 5 / 9;
+    }
+
+    private static double convCelsiusToFahrenheit (double celsius) {
+        return (celsius * 9 / 5) + 32;
     }
 }
